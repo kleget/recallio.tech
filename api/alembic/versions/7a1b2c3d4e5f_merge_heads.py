c2612efbf0a1d309f3cca7f73668ea9aac25286f
kleget_win_notebook <kleget_dev@mail.ru>
2026-01-25 02:42:31 +0300

    Merge alembic heads

---
+++ api/alembic/versions/7a1b2c3d4e5f_merge_heads.py
@@ -0,0 +1,22 @@
+"""merge heads
+
+Revision ID: 7a1b2c3d4e5f
+Revises: d9b8a7c6e5f4, 6c2d1e4f9a0b
+Create Date: 2026-01-25 04:10:00.000000
+"""
+
+from alembic import op
+
+
+revision = "7a1b2c3d4e5f"
+down_revision = ("d9b8a7c6e5f4", "6c2d1e4f9a0b")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
